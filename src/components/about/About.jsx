5e3d860a727d7bf17932ecf0ab379538010bab60
abdus-sabur <absabur929@gmail.com>
2023-10-12 00:36:12 +0600

    about page bug solve

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import "./About.css"
 
 const About = () => {
   return (
-    <div style={{width: "100%", height: "70vh", minWidth: "700px"}}>
+    <div style={{width: "100%",maxWidth: '100vw', height: "70vh", minHeight: "700px"}}>
       <iframe style={{width: "100%", height: "100%"}} id="iFrameExample"
           src="https://absabur.github.io/Abdus_Sabur/">
       </iframe>
